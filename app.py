96daa74404172e8d2de74a9ebd58935617974147
Yorman Blanco <yorman30397167@gmail.com>
2026-06-11 16:35:05 -0400

    Forzar driver psycopg2

--- app.py
+++ app.py
@@ -18,11 +18,8 @@ app = Flask(__name__)
 # ==========================================
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://", 1)
-    elif DATABASE_URL.startswith("postgresql://"):
-        DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+pg8000://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+   
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace('postgresql://', 'postgresql+psycopg2://')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project_data.db'
 
